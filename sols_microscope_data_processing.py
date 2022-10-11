79347034a838eae9d6ee8b04cc104e0b011e540e
Alfred Millett-Sikking <amsikking@gmail.com>
2022-10-11 15:05:49 -0700

    Update sols_microscope_data_processing.py
    Updated to reflect new 'sols_microscope.py' API ('preview_crop_px' etc).

--- sols_microscope_data_processing.py
+++ sols_microscope_data_processing.py
@@ -50,13 +50,16 @@ t1 = time.perf_counter()
 print('(%0.2fs)'%(t1 - t0))
 print('-> data.shape =', data.shape)
 print('-> format = 5D "tzcyx" (volumes, slices, channels, height_px, width_px)')
-scan_step_size_px, preview_line_px, timestamp_mode = 3, 10, "binary+ASCII"
-s_px, l_px, ts = scan_step_size_px, preview_line_px, timestamp_mode # shorthand
+scan_step_size_px = 3
+preview_line_px = 10
+preview_crop_px = 3
+timestamp_mode = "binary+ASCII"
 voxel_aspect_ratio =  1.7320508075688772
 
 # Get preview:
 print('\nGetting: preview', end=' ')
-preview = datapreview.get(data, s_px, l_px, ts)
+preview = datapreview.get(
+    data, scan_step_size_px, preview_line_px, preview_crop_px, timestamp_mode)
 t2 = time.perf_counter()
 print('(%0.2fs)'%(t2 - t1))
 print('-> saving: data_preview.tif')
@@ -64,7 +67,8 @@ imwrite('data_preview.tif', preview)
 
 # Get roi: -> edit 'signal_to_bg_ratio' and 'gaussian_filter_std' as needed
 print('\nGetting: roi', end=' ')
-roi = dataroi.get(data, ts, signal_to_bg_ratio=1.2, gaussian_filter_std=3)
+roi = dataroi.get(data, preview_crop_px, timestamp_mode,
+                  signal_to_bg_ratio=1.2, gaussian_filter_std=3)
 t3 = time.perf_counter()
 print('(%0.2fs)'%(t3 - t2))
 print('-> saving: data_roi.tif')
@@ -72,7 +76,8 @@ imwrite('data_roi.tif', roi)
 
 # Get roi preview:
 print('\nGetting: roi preview', end=' ')
-roi_preview = datapreview.get(roi, s_px, l_px, ts)
+roi_preview = datapreview.get(
+    roi, scan_step_size_px, preview_line_px, preview_crop_px, timestamp_mode)
 t4 = time.perf_counter()
 print('(%0.2fs)'%(t4 - t3))
 print('-> saving: data_roi_preview.tif')
@@ -80,7 +85,7 @@ imwrite('data_roi_preview.tif', roi_preview)
 
 # Get native data:
 print('\nGetting: roi native view', end=' ')
-native = datanative.get(roi, s_px)
+native = datanative.get(roi, scan_step_size_px)
 t5 = time.perf_counter()
 print('(%0.2fs)'%(t5 - t4))
 print('-> saving: data_roi_native.tif')
@@ -89,7 +94,7 @@ imwrite('data_roi_native.tif', native)
 # Get traditional data for roi: -> this is very slow (adds about ~30s)
 print('\nGetting: roi traditional view', end=' ')
 native_subset = native[0:1, : , 0:1, :, :] # -> picking 1 volume, but keep 5D!
-traditional = datatraditional.get(native_subset, s_px)
+traditional = datatraditional.get(native_subset, scan_step_size_px)
 t6 = time.perf_counter()
 print('(%0.2fs)'%(t6 - t5))
 print('-> saving: data_roi_traditional.tif')
